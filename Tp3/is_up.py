daee85d112cc5ea6b529868c673d8c8b33811c73
Matsuel <matsuel0308@gmail.com>
2023-10-26 12:08:56 +0200

    edit is up

--- Tp3/is_up.py
+++ Tp3/is_up.py
@@ -9,7 +9,7 @@ def is_up(args):
         exit()
     else:
         if (os.name=="posix"):
-            response=os.system("ping -c  " + args[1]+" >/dev/null")
+            response=os.system("ping -c 1" + args[1]+" >/dev/null")
         else:
             response = os.system("ping -n 1 " + args[1]+" > nul")
     if response == 0:

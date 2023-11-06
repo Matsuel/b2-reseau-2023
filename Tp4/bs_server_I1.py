122ebe27ebe4704cb5c2628ca940bffb228beec5
Matsuel <matsuel0308@gmail.com>
2023-11-06 09:26:55 +0100

    bs_server_I1.py

---
+++ Tp4/bs_server_I1.py
@@ -0,0 +1,26 @@
+import socket
+
+host = '10.33.79.156'
+port = 13337
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port))  
+
+s.listen(1)
+conn, addr = s.accept()
+print('Hi mate !', addr)
+
+while True:
+
+    try:
+        data = conn.recv(1024)
+        if not data: break
+        print(f"Données reçues du client : {data}")
+        conn.sendall("Salut mec.")
+
+    except socket.error:
+        print("Error Occured.")
+        break
+
+# On ferme proprement la connexion TCP
+conn.close()

f86fb0dbd215c2c99a57cb2648f0b5c1ded3ed69
Matsuel <matsuel0308@gmail.com>
2023-11-29 10:10:40 +0100

    Strings sur mesure

--- Tp5/tp5_enc_client_1.py
+++ Tp5/tp5_enc_client_1.py
@@ -1,10 +1,15 @@
 import socket 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('127.0.0.1', 9999))
 
-# on récup une string saisie par l'utilisateur
-operation= input('Entrer une expression arithmétique simple: ')
+host='127.0.0.1'
+port=9999
+
+try:
+    sock.connect((host,port))
+except:
+    print(f"Impossible de se connecter à {host} sur le port {port} déso bro.")
+    exit(1)
 
 def checkInput(inputUsr:str)->bool:
     if inputUsr.__contains__("+") or inputUsr.__contains__("-") or inputUsr.__contains__("*"):
@@ -21,23 +26,33 @@ def checkInput(inputUsr:str)->bool:
     else:
         return False
 
-if checkInput(operation) :
-    # on encode le message explicitement en UTF-8 pour récup un tableau de bytes
-    encoded_msg = operation.replace(' ', '').encode('utf-8')
+while True:
+    # on récup une string saisie par l'utilisateur
+    operation= input('Entrer une expression arithmétique simple: ')
+    if checkInput(operation) :
+        # on encode le message explicitement en UTF-8 pour récup un tableau de bytes
+        encoded_msg = operation.replace(' ', '').encode('utf-8')
 
-    # on calcule sa taille, en nombre d'octets
-    msg_len = len(encoded_msg)
+        # on calcule sa taille, en nombre d'octets
+        msg_len = len(encoded_msg)
 
-    # on encode ce nombre d'octets sur une taille fixe de 4 octets
-    header = msg_len.to_bytes(4, byteorder='big')
+        # on encode ce nombre d'octets sur une taille fixe de 4 octets
+        header = msg_len.to_bytes(4, byteorder='big')
 
-    # on peut concaténer ce header avec le message, avant d'envoyer sur le réseau
-    payload = header + encoded_msg
+        # on peut concaténer ce header avec le message, avant d'envoyer sur le réseau
+        payload = header + encoded_msg
 
-    sock.send(payload)
-    # on peut envoyer ça sur le réseau  
+        sock.send(payload)
+        print(f"Opération {encoded_msg.decode('utf-8')} envoyé au serveur.")
+        # on peut envoyer ça sur le réseau  
+
+        ope_result= sock.recv(1024).decode()
+
+        print(f"Résultat de l'opération : {ope_result}")
 
 
-    sock.close()
-else:
-    raise RuntimeError('Invalid input bro')
+    else:
+        print('Invalid input bro')
+        continue
+
+sock.close()
--- Tp5/tp5_enc_server_1.py
+++ Tp5/tp5_enc_server_1.py
@@ -1,6 +1,7 @@
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind(('127.0.0.1', 9999))
 sock.listen()
 client, client_addr = sock.accept()
@@ -8,35 +9,44 @@ client, client_addr = sock.accept()
 while True:
     # On lit les 4 premiers octets qui arrivent du client
     # Car dans le client, on a fixé la taille du header à 4 octets
-    header = client.recv(4)
-    if not header:
-        break
+    try:
+        header = client.recv(4)
+        if not header:
+            break
 
-    # On lit la valeur
-    msg_len = int.from_bytes(header[0:4], byteorder='big')
+        # On lit la valeur
+        msg_len = int.from_bytes(header[0:4], byteorder='big')
 
-    print(f"Lecture des {msg_len} prochains octets")
+        print(f"Lecture des {msg_len} prochains octets")
 
-    # Une liste qui va contenir les données reçues
-    chunks = []
+        # Une liste qui va contenir les données reçues
+        chunks = []
 
-    bytes_received = 0
-    while bytes_received < msg_len:
-        # Si on reçoit + que la taille annoncée, on lit 1024 par 1024 octets
-        chunk = client.recv(min(msg_len - bytes_received,
-                                1024))
-        if not chunk:
-            raise RuntimeError('Invalid chunk received bro')
+        bytes_received = 0
+        while bytes_received < msg_len:
+            # Si on reçoit + que la taille annoncée, on lit 1024 par 1024 octets
+            chunk = client.recv(min(msg_len - bytes_received,
+                                    1024))
+            if not chunk:
+                raise RuntimeError('Invalid chunk received bro')
 
-        # on ajoute le morceau de 1024 ou moins à notre liste
-        chunks.append(chunk)
+            # on ajoute le morceau de 1024 ou moins à notre liste
+            chunks.append(chunk)
 
-        # on ajoute la quantité d'octets reçus au compteur
-        bytes_received += len(chunk)
+            # on ajoute la quantité d'octets reçus au compteur
+            bytes_received += len(chunk)
 
-    # ptit one-liner pas combliqué à comprendre pour assembler la liste en un seul message
-    message_received = b"".join(chunks).decode('utf-8')
-    print(f"Received from client {message_received} result {eval(message_received)}")
+        # ptit one-liner pas combliqué à comprendre pour assembler la liste en un seul message
+        message_received = b"".join(chunks).decode('utf-8')
+        print(f"Received from client {message_received} result {eval(message_received)}")
+
+        res= eval(message_received)
+        print("Result send to client")
+        client.send(str(res).encode('utf-8'))
+
+    except socket.error:
+        print("Error occured bro.")
+        exit(1)
 
 client.close()
 sock.close()

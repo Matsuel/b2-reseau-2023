7e157db22b25758e45685c51f19965162ea8b90c
Matsuel <matsuel0308@gmail.com>
2023-12-05 09:09:38 +0100

    sleep nd print

---
+++ Tp6/sleep_and_print.py
@@ -0,0 +1,15 @@
+import asyncio
+
+async def sleep_and_print():
+    for i in range(1,11):
+        print(i)
+        await asyncio.sleep(0.5)
+
+loop = asyncio.get_event_loop()
+
+tasks=[
+    loop.create_task(sleep_and_print()),
+]
+
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
